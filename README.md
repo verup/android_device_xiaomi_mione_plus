41d5b9d7ae45e13695095e566421d6687fc18306
Tommy Alex <iptux7@gmail.com>
2013-07-04 23:22:46 +0800

    graphic in v3.4 kernel is always a pain
    Change-Id: Icaaa8a9086038f9e818a8c5c761ff52dc7949359

---
+++ README.md
@@ -0,0 +1,10 @@
+this branch is an effort to use the v3.4 kernel from xiaomi for **CM10**,
+but failed with no graphic available
+
+you should update some files from latest MIUI before build
+* `prebuilt/kernel`
+* `prebuilt/bcmdhd.ko`
+* `ramdisk/sbin/chargeonlymode`
+
+`jb-k3.4` branch may help
+
--- mione_bcm43xx.mk
+++ mione_bcm43xx.mk
@@ -6,7 +6,7 @@
 
 
 # common bcm43xx
-BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE := bcmdhd
 WIFI_BAND := 802_11_ABG
@@ -16,16 +16,16 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # old mione device have bcm4329 chip (M1, M1CU, M1C1, M1youth)
 WIFI_DRIVER_MODULE_NAME := bcm4329
 WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4329.ko"
-WIFI_DRIVER_MODULE_ARG  := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/system/etc/firmware/nvram.txt"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_PATH_AP  := "/system/etc/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG  := "firmware_path=/system/vendor/firmware/fw_bcm4329.bin nvram_path=/system/vendor/firmware/nvram.txt"
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP  := "/system/vendor/firmware/fw_bcm4329_apsta.bin"
 
 # new mione device have bcm4330 chip (M1S, M1Syouth)
 WIFI_DRIVER_MODULE_NAME_DHD := bcmdhd
 WIFI_DRIVER_MODULE_PATH_DHD := "/system/lib/modules/bcmdhd.ko"
-WIFI_DRIVER_MODULE_ARG_DHD  := "firmware_path=/system/etc/firmware/fw_bcmdhd.bin nvram_path=/system/etc/firmware/4330nvram.txt"
-WIFI_DRIVER_FW_PATH_STA_DHD := "/system/etc/firmware/fw_bcmdhd.bin"
-WIFI_DRIVER_FW_PATH_AP_DHD  := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_ARG_DHD  := "firmware_path=/system/vendor/firmware/fw_bcmdhd.bin nvram_path=/system/vendor/firmware/4330nvram.txt"
+WIFI_DRIVER_FW_PATH_STA_DHD := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_AP_DHD  := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
 
 # flag for xiaomi
 BOARD_USE_XIAOMI_MIONE_WIFI := true
